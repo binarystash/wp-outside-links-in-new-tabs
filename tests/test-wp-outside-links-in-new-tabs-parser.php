d6dee670a519a8ab43ba43edf53e8507a5fa9d29
binarystash <binarystash01@gmail.com>
2015-11-01 19:52:00 +0800

    Fixed teset

--- tests/test-wp-outside-links-in-new-tabs-parser.php
+++ tests/test-wp-outside-links-in-new-tabs-parser.php
@@ -15,7 +15,7 @@ class WP_Outside_Links_In_New_Tabs_ParserTest extends WP_UnitTestCase {
 		$input = '<a href="http://www.binarystash.net">BinaryStash</a>';
 		$expected_output = '<a href="http://www.binarystash.net" target="_blank">BinaryStash</a>';
 		
-		$output = $this->_class_instance->parse($content);
+		$output = $this->_class_instance->parse($input);
 		
 		$this->assertEquals($expected_output,$output);
 		
